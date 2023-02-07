ea816f572825b9d936487519376ba7ee9fcc7f7b
Graceshon JS <grashon.2003@gmail.com>
2023-02-07 18:33:26 +0530

    third commit

---
+++ script1.js
@@ -0,0 +1,11 @@
+document.addEventListener(DOMContentLoaded,() =>{
+document.getElementById('balance').textContent = 'hello' 
+console.log('hello');
+    
+})
+
+
+
+//let balance = 6543
+
+//balanceDiv.innerText = balance
